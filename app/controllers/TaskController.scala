7fa29cf3730fb800f8d8869d7fe7ba06af964735
Andrew Varnerin <andrew@varnerin.info>
2017-02-06 20:10:33 -0500

    format instead of read/write

--- app/controllers/TaskController.scala
+++ app/controllers/TaskController.scala
@@ -13,15 +13,8 @@ import play.api.libs.json._
 class TaskController @Inject() (taskService: TaskService) extends Controller {
 
 
-  implicit val taskWrites: OWrites[Task] = Json.writes[model.Task]
-  implicit val taskReads: Reads[Task] = Json.reads[model.Task]
+  implicit val taskFormat: OFormat[Task] = Json.format[Task]
 
-  /**
-    * Create an Action to render an HTML page with a welcome message.
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
   def index = Action {
     Ok( Json.toJson(taskService.list()))
   }
