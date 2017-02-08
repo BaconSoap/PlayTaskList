e0d6079740e237a8b248e5f31d6d99f0ba8f9e1b
Andrew Varnerin <andrew@varnerin.info>
2017-02-07 21:04:55 -0500

    show tasks on index

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -6,6 +6,8 @@
   TaskCtrl.$inject = ['$http'];
   function TaskCtrl($http) {
     console.log($http);
-    this.blah = "bleh";
+    this.tasks = [];
+    var that = this;
+    $http.get('api/tasks').then(function(tasks){console.log(tasks.data); that.tasks = tasks.data;})
   }
 })();
